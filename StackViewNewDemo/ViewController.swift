bc4028ebc28c5f5a3b2d45b11f6c40923806c185
WeronikaPiotrowska <nimbusek123@gmail.com>
2018-11-09 09:28:40 +0100

    Initial Commit

---
+++ StackViewNewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewNewDemo
+//
+//  Created by Weronika Piotrowska on 09.11.2018.
+//  Copyright © 2018 nimbusek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
